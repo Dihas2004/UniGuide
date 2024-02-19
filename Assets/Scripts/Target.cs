9b1b7ee42343848c03372425d7ee0663cd2764c7
Dihas Liyanage <dihasliyanage42@gmail.com>
2024-02-19 12:53:01 +0530

    Navigation In ground floor
    User can select the destination

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class Target
+{
+    public string Name;
+    public GameObject PositionObject;
+    // Start is called before the first frame update
+    
+}
