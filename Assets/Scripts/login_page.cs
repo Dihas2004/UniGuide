d14b2c198d04f18e49857540988d15fe8bdd810c
IsuruNipun <rajapakshanipun2017@gmail.com>
2024-02-26 17:10:23 +0530

    edited
    some settings

--- Assets/Scripts/login_page.cs
+++ Assets/Scripts/login_page.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -89,6 +88,3 @@ public class AuthenticationManager : MonoBehaviour
         errorText.text = "";
     }
 }
-
-
-
