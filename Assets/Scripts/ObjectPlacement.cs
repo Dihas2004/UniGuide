a074a4d554fbac6da20eade228802156f8d076dc
Koliya03 <senarathnakoliya@gmail.com>
2024-02-29 16:35:22 +0530

    Placing a 3D sample ground floor instead of a cube.
    A sample ground floor(not a completed one) will be placed when you tap the screen .But it will create more than one object(Array of objects) wherever you tap the screen

--- Assets/Scripts/ObjectPlacement.cs
+++ Assets/Scripts/ObjectPlacement.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
-[RequireComponent(requiredComponent: typeof(ARRaycastManager),
-    requiredComponent2: typeof(ARRaycastManager))]
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+
+public class ObjectPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
     private GameObject prefab;
 
-    public ARRaycastManager aRRaycastManager;
+    private ARRaycastManager aRRaycastManager;
     private ARPlaneManager aRPlaneManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
+    // Start is called before the first frame update
     private void Awake()
     {
-        ARRaycastManager raycastManager = GetComponent<ARRaycastManager>();
+        aRRaycastManager = GetComponent<ARRaycastManager>();
         aRPlaneManager = GetComponent<ARPlaneManager>();
-
     }
+
     private void OnEnable()
     {
         EnhancedTouch.TouchSimulation.Enable();
@@ -37,29 +38,21 @@ public class NewBehaviourScript : MonoBehaviour
         EnhancedTouch.Touch.onFingerDown -= FingerDown;
 
     }
+
+
     private void FingerDown(EnhancedTouch.Finger finger)
     {
         if (finger.index != 0) return;
+
         if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
         {
             foreach (ARRaycastHit hit in hits)
             {
                 Pose pose = hit.pose;
                 GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
-                if (aRPlaneManager.GetPlane(hit.trackableId).alignment == PlaneAlignment.HorizontalUp)
-                {
-                    Vector3 position = obj.transform.position;
-                    position.y = 0f;
-                    Vector3 cameraPosition = Camera.main.transform.position;
-                    Vector3 direction = cameraPosition - position;
-                    Quaternion targetRoration = Quaternion.LookRotation(direction);
-                    obj.transform.rotation = targetRoration;
-                }
             }
 
         }
-
-
 
     }
 }
